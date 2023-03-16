487c8bf95e7adb534f83b4234b21803941e3afe3
Julian Ostarek <julianostarek@gmail.com>
2023-03-16 14:06:56 +0100

    Bump swift-argument-parser

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .executable(name: "swiftlintbot", targets: ["SwiftLintBot"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
         // Bump to next stable, see:
