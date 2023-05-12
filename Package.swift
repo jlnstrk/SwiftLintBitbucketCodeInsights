8c8cb598237b813b6c4b8d73cd6ecc7f3dbb26ce
Julian Ostarek <julianostarek@gmail.com>
2023-05-12 17:45:33 +0200

    Update swift-tools, minimum platform version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.8
 import PackageDescription
 
 
 let package = Package(
     name: "swiftlintbot",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v13)
     ],
     products: [
         .executable(name: "swiftlintbot", targets: ["SwiftLintBot"])
